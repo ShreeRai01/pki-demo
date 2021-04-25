487137f3b0a91b2a40c281b5e17ce1a8e27f2ff9
Shree Rai <shrikrishna.rai@f1soft.com>
2021-04-25 13:13:37 +0545

    first commit

---
+++ src/main/java/com/shree/pkidemo/PkiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shree.pkidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shree/pkidemo/PkiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shree.pkidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
